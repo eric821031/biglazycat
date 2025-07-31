d41995e19eae2afbc160fa3662650ca8dc7fd695
BigLazyCat <103044393+eric821031@users.noreply.github.com>
2025-07-31 11:08:32 -0400

    Update youtube-data.js

--- data/youtube-data.js
+++ data/youtube-data.js
@@ -1,6 +1,6 @@
 const categoryMap = {
   "生态": ["Bitcoin", "Ethereum","Cardano","Polkadot","COSMOS"],
-  "系列": ["观点分享系列", "操作技巧系列", "PoS质押教程"],
+  "系列": ["观点分享系列", "操作技巧系列", "安全知识系列", "PoS质押教程"],
   "标签": ["无时效性", "热门视频"]
 };
   
@@ -14,6 +14,7 @@ const videos = [
   { title: "什么是BitcoinOS？作用是什么？Cardano上引入BTC的机制和原理", url: "87_exk03ddY", tags: ["Bitcoin","Cardano","技术原理系列"] },
   { title: "Cardanoscan.io教程！ADA链上数据查询工具教程", url: "1C9Jorgy59M", tags: ["Cardano","操作技巧系列"] },
   { title: "COSMOS的IBC Eureka是什么？万链互联的里程碑！", url: "8WgRrvEnUz4", tags: ["COSMOS","技术原理系列"] },
+  { title: "冷（热）钱包授权风险！使用Revoke.cash 工具撤销不必要的授权！", url: "7LfbWiqkoa4", tags: ["Ethereum","安全知识系列"] },
   { title: "币价暴跌！保持心态的“五句真言”！", url: "SAFrE2U6Sqo", tags: ["观点分享系列","无时效性"] },
   { title: "山寨币牛市来临！谈谈我对于币圈的财富自由的想法", url: "YYzQu__do5s", tags: ["观点分享系列","无时效性"] },
   { title: "牛市赢麻了！我可以辞职炒币吗？", url: "dVqlBaXvCI0", tags: ["观点分享系列","无时效性"] }
